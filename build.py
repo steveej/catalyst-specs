3ef7e791c5d70ea45d9c916e9c2a4d5c6e603815
Stefan Junker <code@stefanjunker.de>
2015-05-05 00:05:02 -0700

    build script

--- build.py
+++ build.py
@@ -3,6 +3,7 @@
 import subprocess
 import os
 import urlgrabber
+import sys
 
 abs_file = os.path.abspath(__file__)
 abs_dir = os.path.dirname(abs_file)
@@ -21,7 +22,8 @@ overlay_url = 'https://github.com/steveeJ/personal-portage-overlay.git'
 if not os.path.exists('overlay'):
     subprocess.call('git clone {} overlay'.format(overlay_url),
             shell=True)
-
+else:
+    subprocess.call('git --worktree=overlay pull', shell=True)
 
 # stage3
 if not os.path.exists('download'):
@@ -43,7 +45,7 @@ for filename, url in urls.items():
         print('downloading {} to {}'.format(url,path))
         urlgrabber.urlgrab(url, filename=path)
     else:
-        print('resuing {}'.format(path))
+        print('reusing {}'.format(path))
 
 
 # run catalyst
@@ -51,4 +53,12 @@ portage_env = {
     'PORTAGE_CONFIGROOT': os.path.join(abs_dir, 'confir')
 } 
 
-#subprocess.call('emaint sync -r steveeJ', shell=True, env=portage_env)
+target = None
+if len(sys.argv) > 0:
+    target = sys.argv[1]
+    if os.path.exists(target):
+        subprocess.call('catalyst -c catalyst.conf \
+                -f {}'.format(target)
+    else:
+        print('{} does not exist.'.format(target)
+
